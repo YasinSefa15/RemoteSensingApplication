0eea23c8703ec0902d187a54b69ce4403e31825d
YasinSefa15 <yasekirman@gmail.com>
2023-12-29 11:27:27 +0300

    two threads available for sending to server

--- sensor_apps.py
+++ sensor_apps.py
@@ -5,13 +5,13 @@ import time
 
 
 class TemperatureSensor:
-    def __init__(self, gateway_host, gateway_port):
+    def __init__(self, gateway_host, temperature_gateway_port):
         self.gateway_host = gateway_host
-        self.gateway_port = gateway_port
+        self.temperature_gateway_port = temperature_gateway_port
 
     def start(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((self.gateway_host, self.gateway_port))
+            s.connect((self.gateway_host, self.temperature_gateway_port))
             while True:
                 temperature = random.uniform(20, 30)
                 timestamp = time.time()
@@ -21,9 +21,9 @@ class TemperatureSensor:
 
 
 class HumiditySensor:
-    def __init__(self, gateway_host, gateway_port):
+    def __init__(self, gateway_host, humidity_gateway_port):
         self.gateway_host = gateway_host
-        self.gateway_port = gateway_port
+        self.humidity_gateway_port = humidity_gateway_port
 
     def start(self):
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
@@ -32,16 +32,17 @@ class HumiditySensor:
                 timestamp = time.time()
                 if humidity > 80:
                     message = f'HUMIDITY|{humidity}|{timestamp}'
-                    s.sendto(message.encode(), (self.gateway_host, self.gateway_port))
+                    s.sendto(message.encode(), (self.gateway_host, self.humidity_gateway_port))
                 time.sleep(1)
 
 
 if __name__ == "__main__":
     gateway_host = 'localhost'
-    gateway_port = 5555  # Bu portu değiştirebilirsiniz, ancak gateway ile aynı olmalı
+    temperature_gateway_port = 5555  # Bu portu değiştirebilirsiniz, ancak gateway ile aynı olmalı
+    humidity_gateway_port = 5556  # Bu portu değiştirebilirsiniz, ancak gateway ile aynı olmalı
 
-    temperature_sensor = TemperatureSensor(gateway_host, gateway_port)
-    humidity_sensor = HumiditySensor(gateway_host, gateway_port)
+    temperature_sensor = TemperatureSensor(gateway_host, temperature_gateway_port)
+    humidity_sensor = HumiditySensor(gateway_host, humidity_gateway_port)
 
     threading.Thread(target=temperature_sensor.start).start()
     threading.Thread(target=humidity_sensor.start).start()
