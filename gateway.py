6a1c937f6b8a91cf907926469eda171cce4c57b8
YasinSefa15 <yasekirman@gmail.com>
2024-01-02 06:01:48 +0300

    Sensörlerin çalışıp/çalışmadığı kontrol ediliyor. Loglama yapılıyor.

--- gateway.py
+++ gateway.py
@@ -14,88 +14,171 @@ from datetime import datetime
 
 # eksik kalanlar:
 # 1. server'a gönderme
-# 2. sensorların çalışıp çalışmadığını kontrol etme
 # 3. sensorların çalışıp çalışmadığını server'a gönderme
-# 4. server'a gönderilen verileri loglama
 
 logging.basicConfig(level=logging.DEBUG, filename='gateway.log', filemode='w')
 logger = logging.getLogger(__name__)
 
 
-def startTCP():
-    s_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s_tcp.bind(('', 5559))
-    s_tcp.listen()
-    print("TCP Server is listening...")
-
-    conn, addr = s_tcp.accept()
-    while True:
-        print('Connected by', addr)
-        data = conn.recv(1024)
-        if not data:
-            print("Connection to gateway data is null. Connection is broken.")
-            break
-        decoded_data = data.decode()
-        logger_message = f'Received_at : {get_time()} ; Temperature Sensor: {decoded_data}'
-        logger.info(logger_message)
-        # print(logger_message) # eğer console'a yazdırmak isterseniz bu satırı açın
-        # server'a gönderilme işlemi yapılacak...
-
-
-
-def startUDP():
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s_udp:
-        s_udp.bind(('localhost', 5556))
-        print("UDP Server is listening...")
-
-        while True:
-            data, addr = s_udp.recvfrom(2048)
-            decoded_data = data.decode()
-            logger_message = f'Received_at : {get_time()} ; Humidity Sensor: {decoded_data}'
-            logger.info(logger_message)
-            # print(logger_message) # eğer console'a yazdırmak isterseniz bu satırı açın
-            # server'a gönderilme işlemi yapılacak...
-            # time.sleep(1)
-
-
-def gateway():
-    # TCP ve UDP işlemlerini aynı anda başlatmak için iki thread oluşturun
-    tcp_thread = threading.Thread(target=startTCP)
-    udp_thread = threading.Thread(target=startUDP)
-
-    # Her iki thread'i başlatın
-    tcp_thread.start()
-    udp_thread.start()
-
-    # Her iki thread'in bitmesini bekleyin
-    tcp_thread.join()
-    udp_thread.join()
-
-
-# gateway ve sunucu arasındaki bağlantı buradan kuruluyor
-def connect_to_server(host, port):
-    # thread oluşturulup bağlanacak..
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((host, port))
-        print(f"Connected to server {host}:{port}")
-
-
-# gateway'den sunucuya veri gönderme işlemi burada yapılıyor
-def send_to_server():
-    # sunucuya buradan gönderilecek, ama bir class oluşturmak daha mı mantıklı olur?
-    # çünkü bağlanılan socket'e erişmek gerekiyor, bu yüzden class yapısı gerekecek
-    # benzeri gateway1.py de var.
-    return
-
-
+# Getting the current time in a formatted way
 def get_time():
     current_time = time.time()
     formatted_time = datetime.fromtimestamp(current_time).strftime('%Y-%m-%d %H:%M:%S')
     return formatted_time
 
 
+# Check if data is received from the temperature sensor
+def check_data_received(last_data_received_time):
+    while True:
+        # If no data is received for 3 seconds, send a 'TEMP SENSOR OFF' message
+        if time.time() - last_data_received_time[0] > 3:
+            logger_message = f'No data received for 3 seconds. Sending "TEMP SENSOR OFF" message. Checked_At :{get_time()}'
+            print(logger_message)
+            logger.debug(logger_message)
+            # Burada server'a 'TEMP SENSOR OFF' mesajını gönderme işlemi yapılacak.
+
+        # Check every 3 seconds
+        time.sleep(3)
+
+
+# Receive data from the temperature sensor
+def receive_tcp_data(conn, last_data_received_time):
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            print("Connection to gateway data is null. Connection is broken.")
+            break
+
+        decoded_data = data.decode()
+        logger_message = f'Received_at : {get_time()} ; Temperature Sensor: {decoded_data}'
+        logger.debug(logger_message)
+        print(logger_message)
+
+        # If data is received, update the last data received time
+        last_data_received_time[0] = time.time()
+
+        # server'a gönderilme işlemi yapılacak...
+
+
+# Check if 'ALIVE' message is received from the humidity sensor
+def check_alive_received(last_alive_received_time):
+    while True:
+        # If no 'ALIVE' message is received for 7 seconds, send a 'HUMIDITY SENSOR OFF' message
+        if time.time() - last_alive_received_time[0] > 7:
+            logger_message = (f'No "ALIVE" message received for 7 seconds. Sending "HUMIDITY SENSOR OFF" message. '
+                              f'Checked_At :{get_time()}')
+            print(logger_message)
+            logger.debug(logger_message)
+            # Burada server'a 'HUMIDITY SENSOR OFF' mesajını gönderme işlemi yapılacak.
+
+        time.sleep(7)
+
+
+# Receive data from the humidity sensor
+def receive_udp_data(s_udp, last_alive_received_time):
+    while True:
+        data, addr = s_udp.recvfrom(2048)
+        decoded_data = data.decode()
+        logger_message = f'Received_at : {get_time()} ; Humidity Sensor: {decoded_data}'
+        logger.debug(logger_message)
+        print("Received UDP: " + logger_message)
+
+        # If 'ALIVE' message is received, update the last alive received time
+        if decoded_data == 'ALIVE':
+            last_alive_received_time[0] = time.time()
+
+        # server'a gönderilme işlemi yapılacak...
+
+
+# Gateway class
+class Gateway:
+    def __init__(self, host, port, temperature_port, humidity_port):
+        self.host = host
+        self.port = port
+        self.temperature_port = temperature_port
+        self.humidity_port = humidity_port
+        self.server_socket = None
+
+    # Start the gateway
+    def start(self):
+        # initialize the sockets
+        threading.Thread(target=self.startTCP).start()
+        threading.Thread(target=self.startUDP).start()
+        # self.connect_to_server()
+
+    # Connect to the server
+    def connect_to_server(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.host, self.port))
+            print(f"Connected to server {self.host}:{self.port}")
+            self.server_socket = s  # Bağlantıyı güncelle
+
+    # Start TCP server
+    def startTCP(self):
+        s_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s_tcp.bind(('', self.temperature_port))
+        s_tcp.listen()
+        print("TCP Server is listening...")
+
+        # Keep track of the last time data was received, this is used to check if the sensor is alive
+        # also, considered the time while waiting for connection establishment
+        last_data_received_time = [time.time()]
+
+        # Create a thread to check if data is received from the sensor
+        check_thread = threading.Thread(target=check_data_received, args=(last_data_received_time,))
+        check_thread.start()
+
+        conn, addr = s_tcp.accept()
+        print('Connected by', addr)
+
+        # Create a thread to receive data from the sensor
+        receive_tcp_thread = threading.Thread(target=receive_tcp_data, args=(conn, last_data_received_time))
+        receive_tcp_thread.start()
+
+    def startUDP(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s_udp:
+            s_udp.bind(('localhost', self.humidity_port))
+            print("UDP Server is listening...")
+
+            # To keep track of the last time 'ALIVE' message was received
+            last_alive_received_time = [time.time()]
+
+            # Create a thread to receive data from the sensor
+            receive_thread = threading.Thread(target=receive_udp_data, args=(s_udp, last_alive_received_time))
+            receive_thread.start()
+
+            # Create a thread to check if 'ALIVE' message is received from the sensor
+            check_thread = threading.Thread(target=check_alive_received, args=(last_alive_received_time,))
+            check_thread.start()
+
+            # Main thread, waits for other threads to finish
+            receive_thread.join()
+            check_thread.join()
+
+            return;
+            while True:
+                data, addr = s_udp.recvfrom(2048)
+                decoded_data = data.decode()
+                logger_message = f'Received_at : {get_time()} ; Humidity Sensor: {decoded_data}'
+                logger.info(logger_message)
+                # print(logger_message) # eğer console'a yazdırmak isterseniz bu satırı açın
+                # server'a gönderilme işlemi yapılacak...
+                # time.sleep(1)
+
+    # gateway'den sunucuya veri gönderme işlemi burada yapılıyor
+    def send_to_server(self, data):
+        try:
+            self.server_socket.sendTo(data.encode(), (self.host, self.port))
+        except (socket.error, AttributeError):
+            print("sunucuya gönderme başarısız.")
+            pass
+
+    # zamanı almak için kullanılıyor
+
+
+# Main function
 if __name__ == "__main__":
     server_host = 'localhost'
     server_port = 8080
-    connect_to_server(host=server_host, port=server_port)
-    gateway()
+    gateway = Gateway(server_host, server_port, temperature_port=5559, humidity_port=5556)
+    gateway.start()
--- sensor_apps.py
+++ sensor_apps.py
@@ -23,49 +23,72 @@ def temperature_sensor():
     host = "localhost"
     port = 5559
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # Create a TCP socket
+    with (socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s):
         s.connect((host, port))
         try:
             while True:
+                # generate a random temperature value between 20 and 30
                 temperature_value = random.uniform(20, 30)
                 message = f"TEMP:{temperature_value},{get_time()}"
+
+                # send the temperature value to the gateway
                 s.sendto(message.encode(), (host, port))
+
+                # log the message
                 logger_message = f'Sent_at : {get_time()} ; Temperature Sensor: {message}'
-                logger.info(logger_message)
-                # print(logger_message) # eğer console'a yazdırmak isterseniz bu satırı açın
+                logger.debug(logger_message)
+                # print(logger_message) # if you want to print to console, uncomment this line
+                # time.sleep(random.uniform(0.5, 6.5)) # random delay between 0.5 and 6.5 seconds to test the sensor
+
+                # 1 second delay
                 time.sleep(1)
         except BrokenPipeError:
-            print("Connection to gateway is broken.")
+            logger_message = f'Connection to gateway is broken.'
+            print(logger_message)
+            logger.debug(logger_message)
 
 
 def humidity_sensor():
     host = "localhost"
     port = 5556
 
+    # Create a UDP socket
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         while True:
+            # generate a random humidity value between 40 and 90
             humidity_value = random.uniform(40, 90)
-            timestamp = time.time()
+
+            timestamp = time.time() # get the current time to check if the sensor is alive
+
+            # create the message and log
             message = f"HUMIDITY:{humidity_value},{get_time()}"
             logger_message = f'Generated_at : {get_time()} ; Humidity Sensor Value: {message}'
             logger.info(logger_message)
-            # print(logger_message) # eğer console'a yazdırmak isterseniz bu satırı açın
+
+            # print(logger_message) # if you want to print to console, uncomment this line
+
+            # send the message if the humidity value is greater than 80 and log
             if humidity_value > 80:
                 s.sendto(message.encode(), (host, port))
+
                 logger_message = f'Sent_at : {get_time()} ; Humidity Sensor Value: {message}'
                 logger.info(logger_message)
-                # print(logger_message)  # eğer console'a yazdırmak isterseniz bu satırı açın
+                # print(logger_message)  # if you want to print to console, uncomment this line
 
             # Send 'ALIVE' message every 3 seconds
             if int(timestamp) % 3 == 0:
                 alive_message = "ALIVE"
                 s.sendto(alive_message.encode(), (host, port))
+
                 logger_message = f'Sent_at : {get_time()} ; Humidity Sensor Message: {alive_message}'
                 logger.info(logger_message)
 
+            # 1 second delay
             time.sleep(1)
 
 
+# Start the threads for the sensors
 def handle_sensors():
     temperature_thread = threading.Thread(target=temperature_sensor)
     humidity_thread = threading.Thread(target=humidity_sensor)
@@ -77,11 +100,13 @@ def handle_sensors():
     humidity_thread.join()
 
 
+# Get the current time in the format of 'YYYY-MM-DD HH:MM:SS'
 def get_time():
     current_time = time.time()
     formatted_time = datetime.fromtimestamp(current_time).strftime('%Y-%m-%d %H:%M:%S')
     return formatted_time
 
 
+# Run the sensors
 if __name__ == "__main__":
     handle_sensors()
